4394fe5d41ce9ccfd4797c00f63b1cd478a7361c
Nilesh <nileshdadab@gmail.com>
2024-10-23 12:59:55 +0530

    Project side bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Projectsidebar from "./Components/ProjectsSideBar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main>
+      <Projectsidebar />
+    </main>
   );
 }
 
---
+++ src/Components/ProjectsSideBar.jsx
@@ -0,0 +1,10 @@
+export default function Projectsidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Projects</button>
+      </div>
+    </aside>
+  );
+}
