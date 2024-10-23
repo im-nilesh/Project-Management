4312d8520f9f7c79b182cd0526359d1e7831ab6a
Nilesh <nileshdadab@gmail.com>
2024-10-23 13:55:37 +0530

    FallBack screen created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
 import Projectsidebar from "./Components/ProjectsSideBar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Projectsidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
