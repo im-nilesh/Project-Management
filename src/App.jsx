dccf8f9825b4f948ede6c619637d5a12e5716ebd
Nilesh <nileshdadab@gmail.com>
2024-10-23 14:10:50 +0530

    no projected selected and buttons working for add new project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import NewProject from "./Components/NewProject";
 import NoProjectSelected from "./Components/NoProjectSelected";
 import Projectsidebar from "./Components/ProjectsSideBar";
 
 function App() {
+  const [projectsState, setprojectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setprojectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Projectsidebar />
-      <NoProjectSelected />
+      <Projectsidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
